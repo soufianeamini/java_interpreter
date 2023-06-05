6f68b6c1eaaabbd84ba9857449030e7a42f7c1e4
Soufiane Amini <soufianeamini2002@gmail.com>
2023-06-05 20:25:58 +0100

    fixed a few issues, lsp seems to work tho

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,16 @@
+class Test {
+    int value;
+    public Test(int value) {
+        this.value = value;
+    }
+    public void printValue() {
+        System.out.println("The value of test is: " + value);
+    }
+}
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello World!");
+        Test test = new Test(5);
+        test.printValue();
     }
 }
