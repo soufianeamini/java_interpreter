2981653e6a94f418d71c1cbbeebc8763ee0a5819
samini <soufianeamini2002@gmail.com>
2023-06-13 18:07:14 +0100

    added bindings to the language

--- src/lox/AstPrinter.java
+++ src/lox/AstPrinter.java
@@ -26,6 +26,12 @@ public class AstPrinter implements Expr.Visitor<String> {
         return parenthesize(expr.operator.lexeme, expr.right);
     }
 
+    @Override
+    public String visitVariableExpr(Expr.Variable expr) {
+        if (expr.name.lexeme == null) return "nil";
+        return expr.name.lexeme;
+    }
+
     private String parenthesize(String name, Expr... exprs) {
         StringBuilder builder = new StringBuilder();
 
