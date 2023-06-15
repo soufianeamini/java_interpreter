68ffb90b10a5d2ae28db363468dbf625fdfeeb89
samini <soufianeamini2002@gmail.com>
2023-06-15 09:22:30 +0100

    ijmplementing functions

--- src/lox/Interpreter.java
+++ src/lox/Interpreter.java
@@ -1,5 +1,6 @@
 package lox;
 
+import java.util.ArrayList;
 import java.util.List;
 
 public class Interpreter implements Expr.Visitor<Object>,
@@ -217,5 +218,23 @@ public class Interpreter implements Expr.Visitor<Object>,
 
         //Unreachable
         return null;
+    }
+
+    @Override
+    public Object visitCallExpr(Expr.Call expr) {
+        Object callee = evaluate(expr.callee);
+
+        List<Object> arguments = new ArrayList<>();
+        for (Expr argument : expr.arguments) {
+            arguments.add(evaluate(argument));
+        }
+
+        if (!(callee instanceof LoxCallable)) {
+            throw new RuntimeError(expr.paren,
+                    "Can only call functions and classes.");
+        }
+
+        LoxCallable function = (LoxCallable)callee;
+        return function.call(this, arguments);
     }
 }
