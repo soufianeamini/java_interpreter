e8a83aa92cc67a6f340a7200786922c55ed9f01e
samini <soufianeamini2002@gmail.com>
2023-06-16 03:32:32 +0100

    added return values, + main.lox file for testing purposes

--- src/lox/Parser.java
+++ src/lox/Parser.java
@@ -42,6 +42,7 @@ public class Parser {
         if (match(FOR)) return forStatement();
         if (match(IF)) return ifStatement();
         if (match(PRINT)) return printStatement();
+        if (match(RETURN)) return returnStatement();
         if (match(WHILE)) return whileStatement();
         if (match(LEFT_BRACE)) return new Stmt.Block(block());
 
@@ -108,6 +109,17 @@ public class Parser {
         Expr value = expression();
         consume(SEMICOLON, "Expected ';' after value.");
         return new Stmt.Print(value);
+    }
+
+    private Stmt returnStatement() {
+        Token keyword = previous();
+        Expr value = null;
+        if (!check(SEMICOLON)) {
+            value = expression();
+        }
+
+        consume(SEMICOLON, "Expected ';' after return value");
+        return new Stmt.Return(keyword, value);
     }
 
     private Stmt varDeclaration() {
--- src/tool/GenerateAst.java
+++ src/tool/GenerateAst.java
@@ -30,6 +30,7 @@ public class GenerateAst {
                             " List<Stmt> body",
                 "If         : Expr condition, Stmt thenBranch, Stmt elseBranch",
                 "Print      : Expr expression",
+                "Return     : Token keyword, Expr value",
                 "Var        : Token name, Expr initializer",
                 "While      : Expr condition, Stmt body"
         ));
